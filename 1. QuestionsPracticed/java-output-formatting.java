90ecdd0da5c4bcba05f48d6cd013bbc342780af2
aka2029 <asaks22@gmail.com>
2020-02-16 09:12:23 +0530

    new commit

---
+++ 1. QuestionsPracticed/java-output-formatting.java
@@ -0,0 +1,26 @@
+/*	https://www.hackerrank.com/challenges/java-output-formatting/problem	
+
+for theory see : https://www.baeldung.com/java-printstream-printf
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+
+            System.out.printf("%-15s%03d%n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
