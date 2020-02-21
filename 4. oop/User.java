160f1dce3a9245de24fa5999dba5e9a8c9a21b09
aka2029 <asaks22@gmail.com>
2020-02-21 10:25:32 +0530

    new day new josh

--- 4. oop/User.java
+++ 4. oop/User.java
@@ -1,15 +1,19 @@
 package com.semanticsquare.oop;
 
 public class User {
+   public int id = 1;
+   
    public void printUserType() {
-	   System.out.println("User");
+      System.out.println("User");
    }
    
    public void saveWebLink() {
-	   System.out.println("User: saveWebLink");
-	   postAReview();
+      System.out.println("User: saveWebLink");
+      //postAReview();
    }
-   public void postAReview() {
-	   System.out.println("User: postAReview");
+   public Review postAReview(String reviewText) {
+      System.out.println("User: postAReview");
+      Review review = new Review(reviewText);
+      return review;
    }
 }
