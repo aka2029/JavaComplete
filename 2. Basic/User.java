e4fe9c183e1d8119de6f7c662547379db9136d04
aka2029 <asaks22@gmail.com>
2020-02-22 11:40:06 +0530

    some modifications

--- 2. Basic/User.java
+++ 2. Basic/User.java
@@ -16,7 +16,7 @@ class User {
      }   
 	 
 	 public static void main(String[] args) {
-		User instructor = new User(1002, "Dheeru", 50000);
+		User instructor = new User(1002, "Akash", 50000);
 		System.out.println("Name: " + instructor.name);
 	 }
 }
