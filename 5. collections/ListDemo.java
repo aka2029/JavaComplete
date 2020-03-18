6d518dba0fc7b44ba61cf44d61b5dec0de8ed5bd
aka2029 <asaks22@gmail.com>
2020-03-18 11:21:54 +0530

    New commit

--- 5. collections/ListDemo.java
+++ 5. collections/ListDemo.java
@@ -1,12 +1,15 @@
 package com.collections;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ListDemo {
 
-	private static void arrayListDemo() {
+	private static List<Integer> arrayListDemo() {		//we needed to return list1 for iterator thus return type had to be changed from void to List<Integer>
 		List<Integer> list1 = new ArrayList<>();
 		// Here instead of List, Collection was written....Collection removed the no.
 		// that was specified
@@ -67,17 +70,64 @@ public class ListDemo {
 		// System.out.println("list3: " + list3);
 		System.out.println("list1: " + list1);
 		
-		for (int element : list1) {
+		
+//commented the below code to prevent ConcurrentModificationException and hence would prevent this using an iterator
+		
+	/*	for (int element : list1) {
 			System.out.println("element: " + element);
 			
 			// Generates ConcurrentModificationException
 			if (element == 9) {
 				list1.remove(Integer.valueOf(element)); // valueOf is used due to its caching
 			}
-		}	
+		}
+	*/
+		return list1;
 	}
-
+	
+	private static void iteratorDemo(List<Integer> list1) {
+		System.out.println("\nInside iteratorDemo ... ");
+				
+		Iterator<Integer> iterator = list1.iterator();
+		while (iterator.hasNext()) {
+			int element = iterator.next();
+			System.out.println("element: " + element);
+			
+			if (element == 9) {
+				iterator.remove();
+				iterator.forEachRemaining(Filter::add);	
+			}
+		}
+		System.out.println("list1: " + list1);
+		
+		//list1.forEach(System.out::println);
+		//list1.forEach(Filter::filter);
+		//list1.forEach(new Filter());	// requires implementing Consumer	
+		
+		// forEachRemaining:
+		// http://mail.openjdk.java.net/pipermail/lambda-dev/2013-June/010221.html
+		// A/C Brian Goetz, he doesn't see much use of this method			
+	}
+	
 	public static void main(String[] args) {
-		arrayListDemo();
+		List<Integer> list1 = arrayListDemo();
+		iteratorDemo(list1);
 	}
+}
+
+class Filter implements Consumer {
+	static void filter(Integer i) {
+		if (i == 1)
+			System.out.println(i);
+	}
+	
+	public void accept(Object i) {		
+		if ((int)i == 1)
+			System.out.println(i);
+	}
+		
+	static void add(Integer i) {
+		System.out.println(i + 7);
+	}
+		
 }
